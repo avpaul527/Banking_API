c7245e8126fe9123ccfdb1b0eb99cb79e88c6636
Alexandria Paul <avpaul527@gmail.com>
2025-01-09 10:36:11 -0500

    first commit

---
+++ src/main/java/com/banking/Banking_API/BankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.banking.Banking_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/Banking_API/BankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.Banking_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
