d09d8d6a135d506268457981195c2394e0ab0661
Quon <k.dayquon@icloud.com>
2025-01-10 15:14:55 -0500

    first push

---
+++ src/main/java/com/banking/Banking_API/model/Bill.java
@@ -0,0 +1,127 @@
+package com.banking.Banking_API.model;
+
+import com.banking.Banking_API.model.Account;
+import com.banking.Banking_API.model.Status;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.security.Timestamp;
+
+@Entity
+@Table(name = "bills")
+public class Bill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bill_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private String payee;
+
+    private String nickname;
+
+    private Timestamp creationDate;
+
+    private Timestamp paymentDate;
+
+    private Integer recurringDate;
+
+    private Timestamp upcomingPaymentDate;
+
+    private Double paymentAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "account_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Account account;
+
+
+
+    public Bill() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getPayee() {
+        return payee;
+    }
+
+    public void setPayee(String payee) {
+        this.payee = payee;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Timestamp getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Timestamp paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Integer getRecurringDate() {
+        return recurringDate;
+    }
+
+    public void setRecurringDate(Integer recurringDate) {
+        this.recurringDate = recurringDate;
+    }
+
+    public Timestamp getUpcomingPaymentDate() {
+        return upcomingPaymentDate;
+    }
+
+    public void setUpcomingPaymentDate(Timestamp upcomingPaymentDate) {
+        this.upcomingPaymentDate = upcomingPaymentDate;
+    }
+
+    public Double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(Double paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
